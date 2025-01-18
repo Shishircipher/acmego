d028e008c71832b31856c01da819ee9a8806b8c3
shishir chaurasiya <shishirchaurasiya17@gmail.com>
2025-01-18 12:56:32 +0000

    logger added

---
+++ log/logger.go
@@ -0,0 +1,57 @@
+package logger
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+var (
+	logFile *os.File
+)
+
+func init() {
+	var err error
+
+	// Create the log directory if it doesn't exist
+	err = os.MkdirAll("./.acmego/log", 0700)
+	if err != nil {
+		log.Fatalf("failed to create log directory: %v", err)
+	}
+
+	// Generate a timestamp for the log file name
+	timestamp := time.Now().Format("2006-01-02_15-04-05") // Example: 2025-01-18_13-45-00
+//	logFileName := fmt.Sprintf("./acmego/log/log_%s.log", timestamp)
+	logFileName := "./.acmego/log/" + timestamp + ".log"
+	// Open the log file with the generated name
+	logFile, err = os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Fatalf("failed to open log file: %v", err)
+	}
+
+	// Set the log output to the file
+	log.SetOutput(logFile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
+
+// Fatal logs a critical error message and exits the program
+func Fatal(format string, v ...interface{}) {
+	log.Printf("FATAL: "+format, v...)
+	os.Exit(1)
+}
+
+// Error logs an error message
+func Error(format string, v ...interface{}) {
+	log.Printf("ERROR: "+format, v...)
+}
+
+// Info logs an informational message
+func Info(format string, v ...interface{}) {
+	log.Printf("INFO: "+format, v...)
+}
+
+// Debug logs a debug message
+func Debug(format string, v ...interface{}) {
+	log.Printf("DEBUG: "+format, v...)
+}
+
